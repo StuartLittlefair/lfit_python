b5937d04c2395007fcdb010c793d639105fd47fc
WildJames343 <wildo543@hotmail.com>
2019-03-03 12:30:33 +0000

    test

--- liveCornerPlot.py
+++ liveCornerPlot.py
@@ -6,319 +6,141 @@ from matplotlib.ticker import ScalarFormatter
 import numpy as np
 import mcmc_utils
 
-from triangle import corner
+try:
+    import triangle
+    # This triangle should have a method corner
+    # There are two python packages with conflicting names
+    getattr(triangle, "corner")
+except (AttributeError, ImportError):
+    # We want the other package
+    import corner as triangle
 
-def hist2d(x, y, bins=20, range=None, weights=None, levels=None, smooth=None,
-           ax=None, color=None, plot_datapoints=True, plot_density=True,
-           plot_contours=True, no_fill_contours=False, fill_contours=False,
-           contour_kwargs=None, contourf_kwargs=None, data_kwargs=None,
-           **kwargs):
-    """
-    Plot a 2-D histogram of samples.
+# def corner(toPlot, labels=None, bins=20):
 
-    Parameters
-    ----------
-    x : array_like[nsamples,]
-       The samples.
+#     # Try filling in labels from pandas.DataFrame columns.
+#     if labels is None:
+#         try:
+#             labels = toPlot.columns
+#         except AttributeError:
+#             pass
 
-    y : array_like[nsamples,]
-       The samples.
+#     # Parse the parameter ranges.
+#     range = [[x.min(), x.max()] for x in toPlot]
+#     # Check for parameters that never change.
+#     m = np.array([e[0] == e[1] for e in range], dtype=bool)
+#     if np.any(m):
+#         raise ValueError(("It looks like the parameter(s) in "
+#                             "column(s) {0} have no dynamic range. ")
+#                             .format(", ".join(map(
+#                                 "{0}".format, np.arange(len(m))[m]))))
 
-    levels : array_like
-        The contour levels to draw.
+#     # Some magic numbers for pretty axis layout.
+#     K = len(toPlot)
+#     factor = 2.0           # size of one side of one panel
+#     lbdim = 0.5 * factor   # size of left/bottom margin
+#     trdim = 0.2 * factor   # size of top/right margin
+#     whspace = 0.05         # w/hspace size
+#     plotdim = factor * K + factor * (K - 1.) * whspace
+#     dim = lbdim + plotdim + trdim
 
-    ax : matplotlib.Axes
-        A axes instance on which to add the 2-D histogram.
-
-    plot_datapoints : bool
-        Draw the individual data points.
-
-    plot_density : bool
-        Draw the density colormap.
-
-    plot_contours : bool
-        Draw the contours.
-
-    no_fill_contours : bool
-        Add no filling at all to the contours (unlike setting
-        ``fill_contours=False``, which still adds a white fill at the densest
-        points).
-
-    fill_contours : bool
-        Fill the contours.
-
-    contour_kwargs : dict
-        Any additional keyword arguments to pass to the `contour` method.
-
-    contourf_kwargs : dict
-        Any additional keyword arguments to pass to the `contourf` method.
-
-    data_kwargs : dict
-        Any additional keyword arguments to pass to the `plot` method when
-        adding the individual data points.
-
-    """
-    if ax is None:
-        ax = pl.gca()
-
-    # Set the default range based on the data range if not provided.
-    if range is None:
-        if "extent" in kwargs:
-            logging.warn("Deprecated keyword argument 'extent'. "
-                         "Use 'range' instead.")
-            range = kwargs["extent"]
-        else:
-            range = [[x.min(), x.max()], [y.min(), y.max()]]
-
-    # Set up the default plotting arguments.
-    if color is None:
-        color = "k"
-
-    # Choose the default "sigma" contour levels.
-    if levels is None:
-        levels = 1.0 - np.exp(-0.5 * np.arange(0.5, 2.1, 0.5) ** 2)
-
-    # This is the color map for the density plot, over-plotted to indicate the
-    # density of the points near the center.
-    density_cmap = LinearSegmentedColormap.from_list(
-        "density_cmap", [color, (1, 1, 1, 0)])
-
-    # This color map is used to hide the points at the high density areas.
-    white_cmap = LinearSegmentedColormap.from_list(
-        "white_cmap", [(1, 1, 1), (1, 1, 1)], N=2)
-
-    # This "color map" is the list of colors for the contour levels if the
-    # contours are filled.
-    rgba_color = colorConverter.to_rgba(color)
-    contour_cmap = [list(rgba_color) for l in levels] + [rgba_color]
-    for i, l in enumerate(levels):
-        contour_cmap[i][-1] *= float(i) / (len(levels)+1)
-
-    # We'll make the 2D histogram to directly estimate the density.
-    try:
-        H, X, Y = np.histogram2d(x.flatten(), y.flatten(), bins=bins,
-                                 range=list(map(np.sort, range)),
-                                 weights=weights)
-    except ValueError:
-        raise ValueError("It looks like at least one of your sample columns "
-                         "have no dynamic range. You could try using the "
-                         "'range' argument.")
-
-    if smooth is not None:
-        if gaussian_filter is None:
-            raise ImportError("Please install scipy for smoothing")
-        H = gaussian_filter(H, smooth)
-
-    # Compute the density levels.
-    Hflat = H.flatten()
-    inds = np.argsort(Hflat)[::-1]
-    Hflat = Hflat[inds]
-    sm = np.cumsum(Hflat)
-    sm /= sm[-1]
-    V = np.empty(len(levels))
-    for i, v0 in enumerate(levels):
-        try:
-            V[i] = Hflat[sm <= v0][-1]
-        except:
-            V[i] = Hflat[0]
-    V.sort()
-    m = np.diff(V) == 0
-    if np.any(m):
-        logging.warning("Too few points to create valid contours")
-    while np.any(m):
-        V[np.where(m)[0][0]] *= 1.0 - 1e-4
-        m = np.diff(V) == 0
-    V.sort()
-
-    # Compute the bin centers.
-    X1, Y1 = 0.5 * (X[1:] + X[:-1]), 0.5 * (Y[1:] + Y[:-1])
-
-    # Extend the array for the sake of the contours at the plot edges.
-    H2 = H.min() + np.zeros((H.shape[0] + 4, H.shape[1] + 4))
-    H2[2:-2, 2:-2] = H
-    H2[2:-2, 1] = H[:, 0]
-    H2[2:-2, -2] = H[:, -1]
-    H2[1, 2:-2] = H[0]
-    H2[-2, 2:-2] = H[-1]
-    H2[1, 1] = H[0, 0]
-    H2[1, -2] = H[0, -1]
-    H2[-2, 1] = H[-1, 0]
-    H2[-2, -2] = H[-1, -1]
-    X2 = np.concatenate([
-        X1[0] + np.array([-2, -1]) * np.diff(X1[:2]),
-        X1,
-        X1[-1] + np.array([1, 2]) * np.diff(X1[-2:]),
-    ])
-    Y2 = np.concatenate([
-        Y1[0] + np.array([-2, -1]) * np.diff(Y1[:2]),
-        Y1,
-        Y1[-1] + np.array([1, 2]) * np.diff(Y1[-2:]),
-    ])
-
-    if plot_datapoints:
-        if data_kwargs is None:
-            data_kwargs = dict()
-        data_kwargs["color"] = data_kwargs.get("color", color)
-        data_kwargs["ms"] = data_kwargs.get("ms", 2.0)
-        data_kwargs["mec"] = data_kwargs.get("mec", "none")
-        data_kwargs["alpha"] = data_kwargs.get("alpha", 0.1)
-        ax.plot(x, y, "o", zorder=-1, rasterized=True, **data_kwargs)
-
-    # Plot the base fill to hide the densest data points.
-    if (plot_contours or plot_density) and not no_fill_contours:
-        ax.contourf(X2, Y2, H2.T, [V.min(), H.max()],
-                    cmap=white_cmap, antialiased=False)
-
-    if plot_contours and fill_contours:
-        if contourf_kwargs is None:
-            contourf_kwargs = dict()
-        contourf_kwargs["colors"] = contourf_kwargs.get("colors", contour_cmap)
-        contourf_kwargs["antialiased"] = contourf_kwargs.get("antialiased",
-                                                             False)
-        ax.contourf(X2, Y2, H2.T, np.concatenate([[0], V, [H.max()*(1+1e-4)]]),
-                    **contourf_kwargs)
-
-    # Plot the density map. This can't be plotted at the same time as the
-    # contour fills.
-    elif plot_density:
-        ax.pcolor(X, Y, H.max() - H.T, cmap=density_cmap)
-
-    # Plot the contour edge colors.
-    if plot_contours:
-        if contour_kwargs is None:
-            contour_kwargs = dict()
-        contour_kwargs["colors"] = contour_kwargs.get("colors", color)
-        ax.contour(X2, Y2, H2.T, V, **contour_kwargs)
-
-    ax.set_xlim(range[0])
-    ax.set_ylim(range[1])
-
-def corner(toPlot, labels=None, bins=20):
-
-    # Try filling in labels from pandas.DataFrame columns.
-    if labels is None:
-        try:
-            labels = toPlot.columns
-        except AttributeError:
-            pass
-
-    # Parse the parameter ranges.
-    range = [[x.min(), x.max()] for x in toPlot]
-    # Check for parameters that never change.
-    m = np.array([e[0] == e[1] for e in range], dtype=bool)
-    if np.any(m):
-        raise ValueError(("It looks like the parameter(s) in "
-                            "column(s) {0} have no dynamic range. ")
-                            .format(", ".join(map(
-                                "{0}".format, np.arange(len(m))[m]))))
-
-    # Some magic numbers for pretty axis layout.
-    K = len(toPlot)
-    factor = 2.0           # size of one side of one panel
-    lbdim = 0.5 * factor   # size of left/bottom margin
-    trdim = 0.2 * factor   # size of top/right margin
-    whspace = 0.05         # w/hspace size
-    plotdim = factor * K + factor * (K - 1.) * whspace
-    dim = lbdim + plotdim + trdim
-
-    # Create a new figure
-    fig, axes = plt.subplots(K, K, figsize=(dim, dim))
+#     # Create a new figure
+#     fig, axes = plt.subplots(K, K, figsize=(dim, dim))
     
-    # Format the figure.
-    lb = lbdim / dim
-    tr = (lbdim + plotdim) / dim
-    fig.subplots_adjust(left=lb, bottom=lb, right=tr, top=tr,
-                        wspace=whspace, hspace=whspace)
+#     # Format the figure.
+#     lb = lbdim / dim
+#     tr = (lbdim + plotdim) / dim
+#     fig.subplots_adjust(left=lb, bottom=lb, right=tr, top=tr,
+#                         wspace=whspace, hspace=whspace)
 
-    # Set up the default histogram keywords.
-    hist_kwargs = dict()
-    hist_kwargs["color"] = hist_kwargs.get("color", 'k')
-    hist_kwargs["histtype"] = hist_kwargs.get("histtype", "step")
+#     # Set up the default histogram keywords.
+#     hist_kwargs = dict()
+#     hist_kwargs["color"] = hist_kwargs.get("color", 'k')
+#     hist_kwargs["histtype"] = hist_kwargs.get("histtype", "step")
 
-    for i, x in enumerate(toPlot):
-        # Deal with masked arrays.
-        if hasattr(x, "compressed"):
-            x = x.compressed()
+#     for i, x in enumerate(toPlot):
+#         # Deal with masked arrays.
+#         if hasattr(x, "compressed"):
+#             x = x.compressed()
 
-        if np.shape(toPlot)[0] == 1:
-            ax = axes
-        else:
-            ax = axes[i, i]
-        # Plot the histograms.
-        n, _, _ = ax.hist(x, bins=bins[i])
+#         if np.shape(toPlot)[0] == 1:
+#             ax = axes
+#         else:
+#             ax = axes[i, i]
+#         # Plot the histograms.
+#         n, _, _ = ax.hist(x, bins=bins[i])
         
-        title = None
-        if labels is not None:
-            title = "{0}".format(labels[i])
+#         title = None
+#         if labels is not None:
+#             title = "{0}".format(labels[i])
 
-        if title is not None:
-            ax.set_title(title)
+#         if title is not None:
+#             ax.set_title(title)
 
-        # Set up the axes.
-        ax.set_xlim(range[i])
-        ax.set_ylim(0, 1.1 * np.max(n))
-        ax.set_yticklabels([])
-        ax.xaxis.set_major_locator(MaxNLocator(5, prune="lower"))
+#         # Set up the axes.
+#         ax.set_xlim(range[i])
+#         ax.set_ylim(0, 1.1 * np.max(n))
+#         ax.set_yticklabels([])
+#         ax.xaxis.set_major_locator(MaxNLocator(5, prune="lower"))
 
-        if i < K - 1:
-            ax.set_xticklabels([])
-        else:
-            [l.set_rotation(45) for l in ax.get_xticklabels()]
-            if labels is not None:
-                ax.set_xlabel(labels[i])
-                ax.xaxis.set_label_coords(0.5, -0.3)
+#         if i < K - 1:
+#             ax.set_xticklabels([])
+#         else:
+#             [l.set_rotation(45) for l in ax.get_xticklabels()]
+#             if labels is not None:
+#                 ax.set_xlabel(labels[i])
+#                 ax.xaxis.set_label_coords(0.5, -0.3)
 
-            # use MathText for axes ticks
-            ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=False))
+#             # use MathText for axes ticks
+#             ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=False))
 
-        for j, y in enumerate(toPlot):
-            if np.shape(toPlot)[0] == 1:
-                ax = axes
-            else:
-                ax = axes[i, j]
-            if j > i:
-                ax.set_frame_on(False)
-                ax.set_xticks([])
-                ax.set_yticks([])
-                continue
-            elif j == i:
-                continue
+#         for j, y in enumerate(toPlot):
+#             if np.shape(toPlot)[0] == 1:
+#                 ax = axes
+#             else:
+#                 ax = axes[i, j]
+#             if j > i:
+#                 ax.set_frame_on(False)
+#                 ax.set_xticks([])
+#                 ax.set_yticks([])
+#                 continue
+#             elif j == i:
+#                 continue
 
-            # Deal with masked arrays.
-            if hasattr(y, "compressed"):
-                y = y.compressed()
+#             # Deal with masked arrays.
+#             if hasattr(y, "compressed"):
+#                 y = y.compressed()
 
-            hist2d(y, x, ax=ax, range=[range[j], range[i]], bins=[bins[j], bins[i]])
+#             hist2d(y, x, ax=ax, range=[range[j], range[i]], bins=[bins[j], bins[i]])
 
             
-            ax.xaxis.set_major_locator(MaxNLocator(5, prune="lower"))
-            ax.yaxis.set_major_locator(MaxNLocator(5, prune="lower"))
+#             ax.xaxis.set_major_locator(MaxNLocator(5, prune="lower"))
+#             ax.yaxis.set_major_locator(MaxNLocator(5, prune="lower"))
 
-            if i < K - 1:
-                ax.set_xticklabels([])
-            else:
-                [l.set_rotation(45) for l in ax.get_xticklabels()]
-                if labels is not None:
-                    ax.set_xlabel(labels[j])
-                    ax.xaxis.set_label_coords(0.5, -0.3)
+#             if i < K - 1:
+#                 ax.set_xticklabels([])
+#             else:
+#                 [l.set_rotation(45) for l in ax.get_xticklabels()]
+#                 if labels is not None:
+#                     ax.set_xlabel(labels[j])
+#                     ax.xaxis.set_label_coords(0.5, -0.3)
 
-                # use MathText for axes ticks
-                ax.xaxis.set_major_formatter(
-                    ScalarFormatter(useMathText=False))
+#                 # use MathText for axes ticks
+#                 ax.xaxis.set_major_formatter(
+#                     ScalarFormatter(useMathText=False))
 
-            if j > 0:
-                ax.set_yticklabels([])
-            else:
-                [l.set_rotation(45) for l in ax.get_yticklabels()]
-                if labels is not None:
-                    ax.set_ylabel(labels[i])
-                    ax.yaxis.set_label_coords(-0.3, 0.5)
+#             if j > 0:
+#                 ax.set_yticklabels([])
+#             else:
+#                 [l.set_rotation(45) for l in ax.get_yticklabels()]
+#                 if labels is not None:
+#                     ax.set_ylabel(labels[i])
+#                     ax.yaxis.set_label_coords(-0.3, 0.5)
 
-                # use MathText for axes ticks
-                ax.yaxis.set_major_formatter(
-                    ScalarFormatter(useMathText=False))
+#                 # use MathText for axes ticks
+#                 ax.yaxis.set_major_formatter(
+#                     ScalarFormatter(useMathText=False))
 
-    return fig
+#     return fig
 
 
 
