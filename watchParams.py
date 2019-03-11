cf970cdb27210ca543106d221a822eaaaa6b659d
WildJames343 <wildo543@hotmail.com>
2019-03-11 11:21:58 +0000

    Padding

--- watchParams.py
+++ watchParams.py
@@ -274,10 +274,18 @@ plotPars.on_change('value', add_plot)
 # I want a toggle to enable or disable the extra params
 complex_button = Toggle(label='Complex BS?', width=120, )
 complex_button.on_click(update_complex)
+complex_button.min_border_right  = 80
+complex_button.min_border_left   = 80
+complex_button.min_border_top    = 80
+complex_button.min_border_bottom = 80
 
 # Ask the user how many eclipses are in the data
 eclipses = Slider(title='How many eclipses?', width=200, start=1, end=10, step=1, value=1)
 eclipses.on_change('value', update_ecl)
+eclipses.min_border_right  = 80
+eclipses.min_border_left   = 80
+eclipses.min_border_top    = 80
+eclipses.min_border_bottom = 80
 
 # Add stuff to the visible area
 layout = gridplot([eclipses, complex_button, plotPars], ncols=2)
