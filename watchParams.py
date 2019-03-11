ad18468b523e2c757b1c352cd4bf61834475af97
WildJames343 <wildo543@hotmail.com>
2019-03-11 08:56:44 +0000

    Updated watchParams to use Bokeh plotting

--- watchParams.py
+++ watchParams.py
@@ -1,336 +1,268 @@
-import matplotlib
-from matplotlib import _pylab_helpers
-from matplotlib.rcsetup import interactive_bk as _interactive_bk
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import matplotlib.animation as animation
-
-from datetime import datetime
-from datetime import timedelta
+import bokeh as bk
+from bokeh.layouts import row, column, gridplot
+from bokeh.models import ColumnDataSource, Band
+from bokeh.plotting import curdoc, figure
+from bokeh.server.callbacks import NextTickCallback
+from bokeh.models.widgets import inputs
+from bokeh.models.widgets.buttons import Toggle
 
 import numpy as np
 
 import sys
-import os
 import time
 
-### Currently, reading in the data takes a long time.
-# Could open the file, store as a global variable, and every x seconds, read in from the last cursor
-# position to the end of the file, and append that to a global data variable?
 
-def pause(interval):
-    """
-    Pause for *interval* seconds.
-    If there is an active figure it will be updated and displayed,
-    and the GUI event loop will run during the pause.
-    If there is no active figure, or if a non-interactive backend
-    is in use, this executes time.sleep(interval).
-    This can be used for crude animation. For more complex
-    animation, see :mod:`matplotlib.animation`.
-    This function is experimental; its behavior may be changed
-    or extended in a future release.
-    """
-    backend = matplotlib.rcParams['backend']
-    if backend in _interactive_bk:
-        figManager = _pylab_helpers.Gcf.get_active()
-        if figManager is not None:
-            canvas = figManager.canvas
-            if canvas.figure.stale:
-                canvas.draw()
-            plt.show(block=False)
-            canvas.start_event_loop(interval)
-            return
+# function that reads in the next step
+def readStep():
+    global f
 
-    # No on-screen figure is active, so sleep() is all we need.
-    time.sleep(interval)
+    stepData = np.zeros((nWalkers, len(pars)))
+    flag = True # If true, return the data. If False, the end of the file was reached before the step was fully read in.
+    init = f.tell()
 
-
-if __name__ == "__main__":
-    '''
-    watchParams <chain file>
-
-    Plots the ln(like), and optionally N parameters, but labels are needed too.
-    '''
-
-    # Get the args passed via command line
-    args = sys.argv[1:]
-    # Check everything is fine
-    try:
-        file = args[0]
-    except:
-        print("Usage: watchParams <chain file>")
-        exit()
-
-    pars   = []
-    labels = []
-    while True:
-        cont = input("Plot parameter evolution [y/n]: ")
-        cont = cont.lower() == 'y'
-
-        if not cont:
-            print("")
+    for i in np.arange(nWalkers):
+        # Get the next line
+        line = f.readline().strip()
+        # Are we at the end of the file?
+        if line == '':
+            # The file is over.
+            flag = False
             break
 
-        if labels == []:
-            necl = input("How many eclipses: ")
-            necl = int(necl)
+        line = np.array(line.split(), dtype=np.float64)
 
-            parNames = ['wdFlux_0', 'dFlux_0', 'sFlux_0', 'rsFlux_0', 'q', 'dphi',\
-                'rdisc_0', 'ulimb_0', 'rwd', 'scale_0', 'az_0', 'fis_0', 'dexp_0', 'phi0_0']
+        # Check for infinities, replace with nans
+        line[np.isinf(line)] = np.nan
 
-            parNameTemplate = ['wdFlux_{0}', 'dFlux_{0}', 'sFlux_{0}', 'rsFlux_{0}',\
-                'rdisc_{0}', 'ulimb_{0}', 'scale_{0}', 'az_{0}', 'fis_{0}', 'dexp_{0}', 'phi0_{0}']
+        # Which walker are we?
+        w = int(line[0])
+        if w != i:
+            print("Walker mismatch!")
+            time.sleep(5)
 
-            # complex has extra parameters
-            complex = input("Complex bright spot? ")
-            if complex.lower()[0] == 'y':
-                parNames.extend(['exp1_0', 'exp2_0', 'tilt_0', 'yaw_0'])
-                parNameTemplate.extend(['exp1_{0}', 'exp2_{0}', 'tilt_{0}', 'yaw_{0}'])
+        # Gather the desired numbers
+        values = line[pars]
+        
+        stepData[w, :] = values
 
-            # Format labels
-            for i in range(necl-1):
-                for name in parNameTemplate:
-                    parNames.append(name.format(i+1))
+    if flag:
+        return stepData
+    else: 
+        # The most recent step wasn't completely read in
+        f.seek(init)
+        
+        for j in range(twait):
+            print("\rWatching for file to updates{: <6} ".format(load[j%len(load)]), end='')
+            time.sleep(0.1)
+        print('\r                                   ', end='\r')
+        return None
 
-            print("The columns go in the following order:")
-            print("{:3d} - {} (ignore)".format(0, 'Walker ID'))
-            for i, name in enumerate(parNames):
-                print("{:3d} - {}".format(i+1, name))
-            print("\nDon't forget to account for unfitted parameters in your input file!\n")
+def update():
+    if labels != []:
+        step = readStep()
 
-        par = input("Which column in the chain file do you want to plot: ")
-        label = input("What label should I apply to this: ")
-
-        par = int(par)
-        if label == '':
-            label = parNames[par-1]
-
-        print("Plotting column {} with the label '{}'\n".format(par, label))
-
-        pars.append(par)
-        labels.append(label)
-
-    twait = input("How long to wait between file reads (s): ")
-    twait = int(twait)
-
-    # fun little loading animation
-    load = ['.', '..', '...']
-    for i in range(5):
-        load.extend(['... -', '... \\', '... |', '... /', '... -', '... \\', '... |', '... /', '... -'])
-    load.extend(['...', '..', '.', '', '', ''])
-
-    # Open the file, and keep it open
-    while True:
-        try:
-            f = open(file, 'r')
-            break
-        except:
-            for j in range(twait*10):
-                print("\rWaiting for file to be created{: <6} ".format(load[j%len(load)]), end='')
-                time.sleep(0.1)
-    print("Opened file OK!                   ")
-
-    # Ideally the code would figure this out
-    # nWalkers = input("How many walkers are you using: ")
-    nWalkers = 0
-    while True:
-        line = f.readline()
-        line = line.split()
-        walker = int(line[0])
-        if nWalkers == walker:
-            nWalkers += 1
+        if step is None:
+            pass
         else:
-            break
-    print("The file has {} walkers...".format(nWalkers))
+            global s
 
+            s += 1
+            means = np.mean(step, axis=0)
+            stds  = np.std(step,  axis=0)
+
+            newdata = dict()
+            newdata['step'] = [s]
+
+            for i, label in enumerate(labels):
+                newdata[label+'Mean'] = [means[i]]
+                newdata[label+'StdUpper']  = [means[i]+stds[i]]
+                newdata[label+'StdLower']  = [means[i]-stds[i]]
+            
+            source.stream(newdata, tail)
+
+def update_ecl(attr, old, new):
+    global necl
+    global parNames
+
+    val = eclipses.value
+    complex = complex_button.active
+    try:
+        necl = int(val)
+        parNames = ['wdFlux_0', 'dFlux_0', 'sFlux_0', 'rsFlux_0', 'q', 'dphi',\
+            'rdisc_0', 'ulimb_0', 'rwd', 'scale_0', 'az_0', 'fis_0', 'dexp_0', 'phi0_0']
+        parNameTemplate = ['wdFlux_{0}', 'dFlux_{0}', 'sFlux_{0}', 'rsFlux_{0}',\
+            'rdisc_{0}', 'ulimb_{0}', 'scale_{0}', 'az_{0}', 'fis_{0}', 'dexp_{0}', 'phi0_{0}']
+
+        # complex has extra parameters
+        if complex:
+            parNames.extend(['exp1_0', 'exp2_0', 'tilt_0', 'yaw_0'])
+            parNameTemplate.extend(['exp1_{0}', 'exp2_{0}', 'tilt_{0}', 'yaw_{0}'])
+
+        # Format labels
+        for i in range(necl-1):
+            for name in parNameTemplate:
+                parNames.append(name.format(i+1))
+
+        parNames.append('Likelihood')
+        selectList = list(parNames)
+        selectList.insert(0, '')
+        plotPars.options = selectList
+    except:
+        eclipses.value = ''
+
+def update_complex(new):
+    global necl
+    global parNames
+
+    val = eclipses.value
+    try:
+        necl = int(val)
+    except:
+        val = 1
+    complex = complex_button.active
+
+    
+    parNames = ['wdFlux_0', 'dFlux_0', 'sFlux_0', 'rsFlux_0', 'q', 'dphi',\
+        'rdisc_0', 'ulimb_0', 'rwd', 'scale_0', 'az_0', 'fis_0', 'dexp_0', 'phi0_0']
+    parNameTemplate = ['wdFlux_{0}', 'dFlux_{0}', 'sFlux_{0}', 'rsFlux_{0}',\
+        'rdisc_{0}', 'ulimb_{0}', 'scale_{0}', 'az_{0}', 'fis_{0}', 'dexp_{0}', 'phi0_{0}']
+
+    # complex has extra parameters
+    # complex = 'n' #input("Complex bright spot? ")
+    if complex:
+        parNames.extend(['exp1_0', 'exp2_0', 'tilt_0', 'yaw_0'])
+        parNameTemplate.extend(['exp1_{0}', 'exp2_{0}', 'tilt_{0}', 'yaw_{0}'])
+
+    # Format labels
+    for i in range(necl-1):
+        for name in parNameTemplate:
+            parNames.append(name.format(i+1))
+
+    parNames.append('Likelihood')
+    selectList = list(parNames)
+    selectList.insert(0, '')
+    plotPars.options = selectList
+
+def add_plot(attr, old, new):
+    global labels
+    global pars
+    global source
+
+    label = str(plotPars.value)
+    if label == '':
+        return
+    par = parNames.index(label) + 1
+    labels.append(label)
+    pars.append(par)
+
+    # Clear data from the source structure 
+    source.data = {'step': []}
+    for label in labels:
+        source.data[label+'Mean']     = []
+        source.data[label+'StdUpper'] = []
+        source.data[label+'StdLower'] = []
+
+    # Move the file cursor back to the beginning of the file
+    global f
+    global s
     f.close()
     f = open(file, 'r')
+    s = 0
 
-    # Interactivity on
-    plt.ion()
-    # Set up plotting area for log(like)
-    likeFig = plt.figure(figsize=[7,4])
-    likeAx = likeFig.add_subplot(111)
-    likeAx.set_title(file)
-    likeAx.set_ylabel('Ln(liklihood)')
-    likeAx.set_xlabel('Iteration')
+    new_plot = bk.plotting.figure(title=label, plot_height=300, plot_width=1200,
+        toolbar_location='above', y_axis_location="right") 
+        # tools="ypan,ywheel_zoom,ybox_zoom,reset")
+    new_plot.line(x='step', y=label+'Mean', alpha=1, line_width=3, color='red', source=source)
+    band = Band(base='step', lower=label+'StdLower', upper=label+'StdUpper', source=source, 
+                level='underlay', fill_alpha=0.5, line_width=0, line_color='black', fill_color='green')
+    new_plot.add_layout(band)
 
-    # Plotting area for parameters to watch
-    if pars != []:
-        paramFig, paramAx = plt.subplots(len(pars), figsize=[7,4], sharex=True)
-        # If we only asked for one parameter to be watched, we need to support indexing still
-        if len(pars) == 1:
-            paramAx = [paramAx]
+    new_plot.x_range.follow = "end"
+    new_plot.x_range.follow_interval = tail
+    new_plot.x_range.range_padding = 0
+    new_plot.y_range.range_padding_units = 'percent'
+    new_plot.y_range.range_padding = 0.2
 
-        paramAx[0].set_title(file)
-        for i, label in enumerate(labels):
-            paramAx[i].set_ylabel(label)
-        paramAx[-1].set_xlabel('Iteration')
+    new_plot.y_range.range_padding_units = 'percent'
+    new_plot.y_range.range_padding = 0.3
 
-    plt.tight_layout()
-    plt.subplots_adjust(hspace=0.0)
-
-    # Open the plotting window
-    plt.show()
-
-    # Tracker variables
-    curline = 1
-    step = 0
-    pstep = 0
-
-    # Limit the amount of memory we use at a time
-    linelimit = int(300000/nWalkers) * nWalkers
-    print("Limited to reading {} lines at a time ({} steps)".format(linelimit, linelimit/nWalkers))
-
-    while True:
-        print("  Reading file...")
-
-        # Reset walkers, dims: (step, walker, parameters)
-        walkers  = np.full((1000, nWalkers, len(pars)+1), np.nan, dtype=np.float64)
-        # For each step, we have an entry for each walker, with slots for each parameter
-        len_walkers = walkers.shape[0]
-
-        # Read to the end of the file
-        nlines = 0          # How many lines have we read on this pass?
-        i = 0               # What index to we store the data to?
-        flag = False
-
-        # Read in the file until we hit the end. If a step wasn't fully read in, discard it.
-        flag = True # If true at the end, wait a while before re-scanning the file. If false, immediately re-scan the file
-        iloc = 0
-
-        newline = True
-        while newline:
-            # Store a set of walkers' individual step
-            temp = np.full((nWalkers, len(pars)+1), np.nan, dtype=np.float64)
-
-            if step%10 == 0:
-                print("\rStep {:5}      ".format(step), end='')
-
-            for j in range(nWalkers):
-                # Get the next line
-                newline = f.readline().strip()
-                # Are we at the end of the file?
-                if newline == '':
-                    # The file is over.
-                    print("Last line read in:\n{}".format(line))
-                    flag = True
-                    break
-                else:
-                    line = newline
-                    nlines += 1
-                    curline += 1
-
-                    # Have we hit the memory limit?
-                    if nlines == linelimit:
-                        print("Hit the line limit! Storing {} walkers".format(j+1))
-                        flag = False
-                        newline = False
-                        break
-
-                line = np.array(line.split(), dtype=np.float64)
-
-                # Check for infinities:
-                line[np.isinf(line)] = np.nan
-
-                # Which walker are we?
-                w = int(line[0])
-                # What step are we up to now?
-                step = np.ceil(curline / nWalkers) - 1
-                step = int(step)
-
-                # Gather the desired numbers
-                lnlike = line[-1]
-                parVals = []
-                for par in pars:
-                    parVals.append(line[par])
-                values = [lnlike] + parVals
-                values = np.array(values)
+    curdoc().add_root(row(new_plot))
 
 
-                temp[j, :] = values
+# Filename
+file = 'chain_prod.txt'
+# Initial values
+necl = 1
+parNames = []
+parNameTemplate = []
 
+# Lists of what parameters we want to plot
+pars   = []
+labels = []
 
-            # Is our array going to be big enough?
-            if iloc == len_walkers-1:
-                shape = list(walkers.shape)
-                newShape = list(shape)
-                newShape[0] += 1000
-                # print("  Expanding array from {} to {}...".format(shape, newShape))
+# fun loading animation
+load = ['.', '..', '...']
+for i in range(5):
+    load.extend(['... -', '... \\', '... |', '... /', '... -', '... \\', '... |', '... /', '... -'])
+load.extend(['...', '..', '.'])
+load.extend(['', '', '', '', '']*4)
+twait = len(load)
 
-                # nans are ignored when we ask for the mean and standard deviation
-                new_array = np.full(newShape, np.nan, dtype=np.float64)
-                # Inject the old data into the new array
-                new_array[0:len_walkers, :, :] = walkers
+# Open the file, and keep it open
+while True:
+    try:
+        f = open(file, 'r')
+        break
+    except:
+        for j in range(300):
+            print("\rWaiting for file to be created{: <6} ".format(load[j%len(load)]), end='')
+            time.sleep(0.1)
+print("Opened file OK!                      ")
 
-                # Copy the array into the old variable
-                walkers = np.array(new_array)
-                len_walkers = walkers.shape[0]
-                # Free up memory
-                del new_array
+# Determine the number of walkers
+nWalkers = 0
+while True:
+    line = f.readline()
+    line = line.split()
+    walker = int(line[0])
+    if nWalkers == walker:
+        nWalkers += 1
+    else:
+        break
+print("The file has {} walkers.".format(nWalkers))
 
-            walkers[iloc, :, :] = temp
-            iloc += 1
-            curstep = step
+# Close and reopen the file to move the cursor back to the beginning.
+f.close()
+# We're at step 0 now
+f = open(file, 'r')
+s = 0
 
+# Initialise data storage
+source = ColumnDataSource(dict(step=[]))
+for label in labels:
+    source.add(data=[], name=label+'Mean')
+    source.add(data=[], name=label+'StdUpper')
+    source.add(data=[], name=label+'StdLower')
 
-        print("Read in {} lines, or {:.2f} steps!".format(nlines, nlines/nWalkers))
-        print("I want to plot from step {} to step {}".format(pstep, curstep))
-        print("Slicing the first {} steps from the walkers".format(iloc))
-        chisqs = walkers[:iloc,:,0]
-        print(chisqs.shape)
+# Initialise the likelihood plotting area
+tail = 2500
 
-        meanChisq = np.nanmean(chisqs, axis=1, dtype=np.float64)
-        stdChisq  = np.nanstd(chisqs, axis=1, dtype=np.float64)
+# Drop down box that should add extra parameters to the plot
+selectList = list(parNames)
+selectList.insert(0, '')
+plotPars = inputs.Select(width=240, title='Optional Parameters', options=selectList, value='')
+plotPars.on_change('value', add_plot)
 
-        N = meanChisq.shape[0]
-        Xrange = np.arange(pstep, pstep+N)
+# I want a toggle to enable or disable the extra params
+complex_button = Toggle(label='Complex BS?', width=120)
+complex_button.on_click(update_complex)
 
-        # Tighten the range
-        likeAx.set_xlim(0, curstep)
-        # Plot data
-        likeAx.fill_between(Xrange,
-                            meanChisq+stdChisq,
-                            meanChisq-stdChisq,
-                            color='green', alpha=0.3
-                            )
-        likeAx.plot(Xrange, meanChisq, color='red')
+# Ask the user how many eclipses are in the data
+eclipses = inputs.TextInput(title='How many eclipses?', width=150)
+eclipses.on_change('value', update_ecl)
 
-        for j, par in enumerate(pars):
-            chisqs = walkers[:iloc, :, j+1]
-
-            meanChisq = np.nanmean(chisqs, axis=1, dtype=np.float64)
-            stdChisq  = np.nanstd(chisqs, axis=1, dtype=np.float64)
-
-            paramAx[j].set_xlim(0, curstep)
-
-            # Plot data
-            paramAx[j].fill_between(Xrange,
-                                meanChisq+stdChisq,
-                                meanChisq-stdChisq,
-                                color='green', alpha=0.3
-                                )
-            paramAx[j].plot(Xrange, meanChisq, color='red')
-
-
-        likeFig.tight_layout()
-        likeFig.canvas.draw_idle()
-        if pars != []:
-            paramFig.tight_layout()
-            paramFig.canvas.draw_idle()
-
-        pstep = curstep
-
-        del walkers
-
-
-        if flag:
-            # Now we want to wait a while.
-            now = datetime.now()
-            t_next = now + timedelta(seconds=twait)
-            print("Reading file next at {}".format(t_next.strftime("%H:%M:%S")))
-            plt.pause(twait)
-        else:
-            plt.pause(0.1)
-
-    f.close()
+# Add stuff to the visible area
+layout = gridplot([eclipses, complex_button, plotPars], ncols=2)
+curdoc().add_root(layout)
+curdoc().title = 'MCMC Chain Supervisor'
+curdoc().add_periodic_callback(update, 1)
