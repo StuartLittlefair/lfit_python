de7a589876c92d4c4f86cc10d0f4dc3a8804b46e
wildjames <jwild2@sheffield.ac.uk>
2019-04-11 01:20:19 -0400

    Trying to embed the server in html

--- watchParams.py
+++ watchParams.py
@@ -342,6 +342,8 @@ class Watcher():
         self.check_file = self.doc.add_periodic_callback(self.open_file, 1000)
 
         print("Finished initialising the dashboard!")
+        print("This is the HTML tag to embed this plot:")
+        print("{}".format(bk.embed.server_session()))
 
     def parse_mcmc_input(self):
         '''Parse the mcmc input dict, and store the following:
