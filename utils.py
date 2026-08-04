441360fc22982c25a911e6abdbeff9a26ccff59f
Stuart Littlefair <s.littlefair@shef.ac.uk>
2026-08-04 15:49:01 +0100

    allow reading of pocoMC chain

--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
-import numpy as np
-import pandas as pd
 import emcee
 import h5py
+import numpy as np
+import pandas as pd
 
 
 def read_chain(chain_fname):
     """
-    Reads an old style text chain or a new style HDF5 chain
+    Reads an old style text chain, and emcee HDF5 chain or a new style pocoMC HDF5 chain
 
     Returns pandas DataFrame and list of variable names
     """
@@ -14,15 +14,46 @@ def read_chain(chain_fname):
         df = pd.read_csv(chain_fname, sep=r"\s+")
         colKeys = list(df.columns.values)[1:-1]
     except UnicodeDecodeError:
-        reader = emcee.backends.HDFBackend(chain_fname, read_only=True)
-        samples = reader.get_chain(discard=0, flat=True, thin=1)
-        nwalkers, npars = reader.shape
-        nsamples = samples.size // npars // nwalkers
         with h5py.File(chain_fname, "r") as f:
-            colKeys = list(f["mcmc"].attrs["var_names"])
+            pocoMC = "weights" in f
+
+        if pocoMC:
+            # pocoMC chain
+            colKeys, df = read_pocoMC_chain(chain_fname)
+        else:
+            # emcee chain
+            colKeys, df = read_emcee_chain(chain_fname)
+
+    return colKeys, df
+
+
+def read_pocoMC_chain(chain_fname):
+    with h5py.File(chain_fname, "r") as f:
+        colKeys = [n.decode() for n in f["var_names"]]
+        samples = np.array(f["samples"])
+        weights = np.array(f["weights"])
+        ln_prob = np.array(f["logp"])
+
+    # weight samples
+    idx_resampled = np.random.choice(
+        np.arange(len(weights)), size=len(samples), replace=True, p=weights
+    )
+    samples = samples[idx_resampled]
+    ln_prob = ln_prob[idx_resampled]
+    df = pd.DataFrame(samples, columns=colKeys)
+    df["ln_prob"] = ln_prob
+    return colKeys, df
+
+
+def read_emcee_chain(chain_fname):
+    reader = emcee.backends.HDFBackend(chain_fname, read_only=True)
+    samples = reader.get_chain(discard=0, flat=True, thin=1)
+    nwalkers, npars = reader.shape
+    nsamples = samples.size // npars // nwalkers
+    with h5py.File(chain_fname, "r") as f:
+        colKeys = list(f["mcmc"].attrs["var_names"])
         df = pd.DataFrame(samples, columns=colKeys)
         df["ln_prob"] = reader.get_log_prob(discard=0, flat=True, thin=1)
         nsamples = samples.size // npars // nwalkers
         df["walker_no"] = np.array(list(range(nwalkers)) * nsamples)
-
     return colKeys, df
